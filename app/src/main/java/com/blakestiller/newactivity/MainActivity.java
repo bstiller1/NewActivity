220e439f2298ef177a601f8fbb7e3ac07dab4e71
Blake Stiller <stiller.blake@gmail.com>
2015-06-22 16:23:36 -0400

    New Activity
    New Activity Example, added ActionBar

--- app/src/main/java/com/blakestiller/newactivity/MainActivity.java
+++ app/src/main/java/com/blakestiller/newactivity/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
